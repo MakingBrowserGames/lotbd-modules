bd0c34bfca429e4e8644485d2a6668dc7ef96ac6
Stephen Kise <stephenrkise@gmail.com>
2016-09-04 21:47:34 -0400

    Creation Color
    Created the module needed to have flat colors upon character creation.

--- creationcolor.php
+++ creationcolor.php
@@ -9,16 +9,63 @@ function creationcolor_getmoduleinfo()
         'category' => 'Account',
         'description' =>
             'Allow players to choose their name color when they create a character.',
+        'download' => 'nope',
         'prefs' => [
             'color' => 'Color the player chose upon account creation, viewonly| `&',
         ]
     ];
+    return $info;
 }
 
 function creationcolor_install()
 {
-    module_addhook('header-user'); // Check if 'op' == 'saved' and if the $_POST contains 'name'. If it has been changed, and doesn't contain color, add the user's chosen color.
-    module_addhook('create-form'); // Ability to pick color. (Probably from a color picker, like on twitch?)
-    module_addhook('process-create'); // Check the player's color, and set it to their account immediately in the prefs and $session['user']['name']
+    module_addhook('everyhit-loggedin');
+    module_addhook('create-form');
+    module_addhook('check-create');
+    module_addhook('process-create');
     return true;
+}
+
+function creationcolor_uninstall()
+{
+    return true;
+}
+
+function creationcolor_dohook($hook, $args)
+{
+    switch ($hook) {
+        case 'everyhit-loggedin':
+            require_once('lib/names.php');
+            global $session;
+            if (substr(get_player_basename(), 0, 1) != '`') {
+                $session['user']['name'] = change_player_name(
+                    $color = get_module_pref('color') . get_player_basename()
+                );
+                debuglog(sprintf("`2Default name color was missing. Automatically changed player base name to %s`2.", $color));
+            }
+            break;
+        case 'create-form':
+            rawoutput("<label for='color'>Choose your default color: </label> <select name='color'>");
+            rawoutput("<option value='`!'>Blue</option>");
+            rawoutput("<option value='`@'>Green</option>");
+            rawoutput("<option value='`#'>Cyan</option>");
+            rawoutput("<option value='`$'>Red</option>");
+            rawoutput("<option value='`%'>Magenta</option>");
+            rawoutput("<option value='`^'>Yellow</option>");
+            rawoutput("<option value='`&' selected>White</option>");
+            rawoutput("<option value='`)'>Gray</option>");
+            rawoutput("</select><br/><br/>");
+            break;
+        case 'check-create':
+            $possibleColors = ['`!', '`@', '`#', '`$', '`%', '`^', '`&', '`)'];
+            if (!in_array(httppost('color'), $possibleColors)) {
+                $args['message'] = 'You must choose a proper color!';
+                $args['blockaccount'] = true;
+            }
+            break;
+        case 'process-create':
+            set_module_pref('color', httppost('color'), 'creationcolor', $args['acctid']);
+            break;
+    }
+    return $args;
 }
