56b97b4445ea561682599415957c82651f763f28
Stephen Kise <stephenrkise@gmail.com>
2016-09-18 13:26:08 -0400

    Items system
    constructed the base schema for the items system, including the
    inventory and bazaar tables as well, as they are a part of the items
    core. The tables for the items system have not been constructed entirely
    yet.

--- items.php
+++ items.php
@@ -20,8 +20,47 @@ function items_getmoduleinfo()
 
 function items_install()
 {
+	$items = [
+		'items' => [
+			'id' => ['name' => 'id', 'type' => 'int(11) unsigned', 'extra' => 'auto_increment'],
+			'name' => ['name' => 'name', 'type' => 'varchar(50)', 'default' => 'Shiny Rock'],
+			'value' => ['name' => 'value', 'type' => 'int(11) unsigned', 'default' => '1'],
+			'value-modifier' => ['name' => 'value-modifier', 'type' => 'decimal(3,2)', 'default' => '1.02'],
+			'ingredients' => ['name' => 'ingredients', 'type' => 'json', 'default' => '[]'],
+			'craft' => ['name' => 'craft', 'type' => 'json', 'default' => '[]'],
+			'weight' => ['name' => 'weight', 'type' => 'decimal(3,2)', 'default' => '0.05'],
+			'rarity' => ['name' => 'rarity', 'type' => 'smallint unsigned', 'default' => '0'],
+			'stat-modifier' => ['name' => 'stat-modifier', 'type' => 'json', 'default' => '[]'], //ex: {"attack": 15, "defense": 2}
+			'functions' => ['name' => 'functions', 'type' => 'json', 'default' => '[]'],//ex: {equippable, non-tradeable, special-attack: dark-light (opens data from dark-light module (create hook in battles for special attacks.))} 
+			'key-PRIMARY' => ['name' => 'PRIMARY', 'type' => 'primary key', 'unique' => '1', 'columns' => 'id'],
+			'key-name' => ['name' => 'name', 'type' => 'key', 'columns' => 'name'],
+			'key-value' => ['name' => 'value', 'type' => 'key', 'columns' => 'value'],
+			'key-value-modifier' => ['name' => 'value-modifier', 'type' => 'key', 'columns' => 'value-modifier'],
+			'key-weight' => ['name' => 'weight', 'type' => 'key', 'columns' => 'weight'],
+			'key-rarity' => ['name' => 'rarity', 'type' => 'key', 'columns' => 'rarity'],
+		],
+		'inventory' => [
+			'acctid' => ['name' => 'acctid', 'type' => 'int(11) unsigned', 'default' => 'NULL'],
+			'max-slots' => ['name' => 'max-slots', 'type' => 'smallint unsigned', 'default' => '12'],
+			'inventory' => ['name' => 'inventory', 'type' => 'json', 'default' => '[]'],
+			'special-attack' => ['name' => 'special-attack', 'type' => 'json', 'default' => '[]'], //ex: {saber-attack, dark-light}
+			'key-PRIMARY' => ['name' => 'PRIMARY', 'type' => 'primary key', 'unique' => '1', 'columns' => 'acctid'],
+			'key-max-slots' => ['name' => 'max-slots', 'type' => 'key', 'columns' => 'max-slots'],
+		],
+		'bazaar' => [
+			'acctid' => ['name' => 'acctid', 'type' => 'int(11) unsigned', 'default' => 'NULL'],
+			'name' => ['name' => 'name', 'type' => 'varchar(128)', 'default' => 'A small stand'],
+			'advertisement' => ['name' => 'advertisement', 'type' => 'varchar(256)', 'default' => 'Buy from us, pls.'],
+			'for-sale' => ['name' => 'for-sale', 'type' => 'json', 'default' => '[]'],
+			'staff' => ['name' => 'staff', 'type' => 'json', 'default' => '[]'],
+			'key-PRIMARY' => ['name' => 'PRIMARY', 'type' => 'primary key', 'unique' => '1', 'columns' => 'acctid'],
+			'key-name' => ['name' => 'name', 'type' => 'key', 'columns' => 'name'],
+			'key-advertisement' => ['name' => 'name', 'type' => 'key', 'columns' => 'advertisement'],
+		]
+	];
     module_addhook('newday');
     module_addhook('superuser');
+	module_addhook('footer-creatures'); // Add drops to creatures.
     return true;
 }
 
