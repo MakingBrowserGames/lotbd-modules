a3408c7932756d6fd2cad9db2d3cca8611971e7b
Stephen Kise <stephenrkise@gmail.com>
2016-09-18 12:54:22 -0400

    Git
    added remote run functions to pull updates to the core, or sync the
    modules and log their messages. This is mostly to benefit from webhooks.

--- git.php
+++ git.php
@@ -13,6 +13,8 @@ function git_getmoduleinfo()
             'changelog' => '0.1b |Stephen Kise, nope',
         ],
         'download' => 'nope',
+        'allowanonymous' => true,
+        'override_forced_nav' => true,
     ];
     return $info;
 }
@@ -42,7 +44,7 @@ function git_dohook($hook, $args)
                 require_once('lib/gamelog.php');
                 if (httpget('git') == 'pull') {
                     shell_exec('git pull');
-                    $output = shell_exec('git log --format="%b (<a href=\"http://github.com/stephenKise/Legend-of-the-Green-Dragon/commit/%h\">%h</a>)" -1');
+                    $output = shell_exec('git log -1 --format="%b (<a href=\"http://github.com/stephenKise/Legend-of-the-Green-Dragon/commit/%h\">%h</a>)"');
                     if ($output != $row['message']) {
                         gamelog($output, get_module_setting('category', 'changelog'));
                     }
@@ -51,4 +53,32 @@ function git_dohook($hook, $args)
             break;
     }
     return $args;
+}
+
+function git_run()
+{
+    $op = httpget('op');
+    $gamelog = db_prefix('gamelog');
+    $sql = db_query("SELECT message FROM $gamelog ORDER BY logid+0 DESC LIMIT 1");
+    $row = db_fetch_assoc($sql);
+    page_header();
+    if (httpget('op') == 'pull_modules') {
+        $exec = shell_exec('git submodule sync');
+        var_dump($exec);
+    }
+    switch ($op) {
+        case 'pull_modules':
+                shell_exec('git submodule sync');
+                $exec = shell_exec('cd modules && git log -1 --format="%b (<a href=\"http://github.com/stephenKise/xythen-modules/commit/%h\">%h</a>)"');
+            break;
+        case 'pull_core':
+                shell_exec('git pull');
+                $exec = shell_exec('git log -1 --format="%b (<a href=\"http://github.com/stephenKise/Legend-of-the-Green-Dragon/commit/%h\">%h</a>)"');
+            break;
+    }
+    if ($exec != $row['message']) {
+        require_once('lib/gamelog.php');
+        gamelog($output, get_module_setting('category', 'changelog'));
+    }
+    page_footer();
 }
