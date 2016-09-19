c5bcf0ad1e94428bfd400fb70a9092dbd0a0f9d1
Stephen Kise <stephenrkise@gmail.com>
2016-09-19 07:43:40 -0400

    Git
    made the git module only log messages - cron will handle any pulls from
    remote.

--- git.php
+++ git.php
@@ -13,8 +13,6 @@ function git_getmoduleinfo()
             'changelog' => '0.1b |Stephen Kise, nope',
         ],
         'download' => 'nope',
-        'allowanonymous' => true,
-        'override_forced_nav' => true,
     ];
     return $info;
 }
@@ -35,52 +33,23 @@ function git_dohook($hook, $args)
     switch ($hook) {
         case 'superuser':
             global $session;
-            $gamelog = db_prefix('gamelog');
-            $sql = db_query("SELECT message FROM $gamelog ORDER BY logid+0 DESC LIMIT 1");
-            $row = db_fetch_assoc($sql);
             if ($session['user']['superuser'] & SU_MANAGE_MODULES) {
-                addnav('Mechanics');
-                addnav('Git Pull', 'superuser.php?git=pull');
-                require_once('lib/gamelog.php');
-                if (httpget('git') == 'pull') {
-                    shell_exec('git pull');
-                    $output = shell_exec('git log -1 --format="%b (<a href=\"http://github.com/stephenKise/Legend-of-the-Green-Dragon/commit/%h\">%h</a>)"');
-                    if ($output != $row['message']) {
-                        gamelog($output, get_module_setting('category', 'changelog'));
-                    }
+                $category = get_module_setting('category', 'changelog');
+                $gamelog = db_prefix('gamelog');
+                $core = shell_exec('git log -1 --format="%b (<a href=\"http://github.com/stephenKise/Legend-of-the-Green-Dragon/commit/%h\">%h</a>)"');
+                $sql = db_query("SELECT logid FROM $gamelog WHERE message = '$core' LIMIT 1");
+                if (db_num_rows($sql) > 0) {
+                    require_once('lib/gamelog.php');
+                    gamelog($core, $category);
+                }
+                $modules = shell_exec('cd modules && git log -1 --format="%b (<a href=\"http://github.com/stephenKise/xythen-modules/commit/%h\">%h</a>)"');
+                $sql = db_query("SELECT logid FROM $gamelog WHERE message = '$modules' LIMIT 1");
+                if (db_num_row($sql) > 0) {
+                    require_once('lib/gamelog.php');
+                    gamelog($modules, $category);
                 }
             }
             break;
     }
     return $args;
-}
-
-function git_run()
-{
-    $op = httpget('op');
-    $gamelog = db_prefix('gamelog');
-    page_header();
-    switch ($op) {
-        case 'pull_modules':
-                shell_exec('git submodule update --remote --recursive');
-                $exec = shell_exec('cd modules && git log -1 --format="%b (<a href=\"http://github.com/stephenKise/xythen-modules/commit/%h\">%h</a>)"');
-            break;
-        case 'pull_core':
-                shell_exec('git pull');
-                $exec = shell_exec('git log -1 --format="%b (<a href=\"http://github.com/stephenKise/Legend-of-the-Green-Dragon/commit/%h\">%h</a>)"');
-            break;
-    }
-    output($exec);
-    $sql = db_query("SELECT message FROM $gamelog WHERE message = '$exec' ORDER BY logid+0 DESC LIMIT 1");
-    if (db_num_rows($sql) != 1) {
-        $row = db_fetch_assoc($sql);
-        require_once('lib/gamelog.php');
-        gamelog($exec, get_module_setting('category', 'changelog'));
-    }
-    if ($_POST['payload']) {
-        require_once('lib/gamelog.php');
-        gamelog(json_encode($_POST['payload'], true), get_module_setting('category', 'changelog'));
-    }
-
-    page_footer();
 }
