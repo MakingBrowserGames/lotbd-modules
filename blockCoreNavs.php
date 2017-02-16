e8adeb54272b74a954b83f51a5eb0333a65efb64
Stephen Kise <stephenrkise@gmail.com>
2017-02-15 22:30:26 -0500

    Added Block Core Navs
    added a module that will help us easily manage which links are blocked.

---
+++ blockCoreNavs.php
@@ -0,0 +1,40 @@
+<?php
+
+function blockCoreNavs_getmoduleinfo()
+{
+    return [
+        'name' => 'Core Blocknavs',
+        'author' => 'Stephen Kise',
+        'version' => '1.0',
+        'category' => 'Administrative',
+        'description' => 'Removes some of core navs that are not necessary',
+        'settings' => [
+            'block' => 'Which links should we block?, textarea| Separate with commas!'
+        ]
+    ];
+}
+
+function blockCoreNavs_install()
+{
+    module_addhook('everyhit');
+    return true;
+}
+
+function blockCoreNavs_uninstall()
+{
+    return true;
+}
+
+function blockCoreNavs_dohook($hook, $args)
+{
+    $settings = get_module_setting('block');
+    $list = explode(',', $settings);
+    if (count($list) <= 1) {
+        blocknav($list[0]);
+        return $args;
+    }
+    foreach ($list as $key => $nav) {
+        blocknav(trim($nav));
+    }
+    return $args;
+}
